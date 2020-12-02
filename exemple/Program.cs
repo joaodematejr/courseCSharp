a48838d55992e0c920cec45de647b9be72c0e330
João Dematé Jr <joaodematejr@MacBook-Pro-de-Joao.local>
2020-12-02 15:37:39 -0300

    Convenções para nomes

---
+++ exemple/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Curso
+{
+    class ContaBancaria
+    {
+        public string Titular {get; set}
+        public double _saldo;
+
+        public void Deposito(double quantia)
+        {
+            _saldo += quantia;
+        }
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exemplo");
+        }
+    }
+}
