bc93861c225ef221288a88c66e1e1743825fbfbd
João Dematé Jr <joaodematejr@MacBook-Pro-de-Joao.local>
2020-12-09 19:59:09 -0300

    Operadores

---
+++ operadores/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // =  a = 10; a Recebe 10
+            // += a += 2; a Recebe a + 2;
+            // -= a -= 2; a Recebe a - 2;
+            // /= a /= 2; a Recebe a / 2;
+            // %= a %= 3; a Recebe a % 3;
+
+            int a = 10;
+            a++;
+            //int b = a++;
+            Console.WriteLine(a);//11
+            a--;
+            Console.WriteLine(a);//10
+        }
+    }
+}
