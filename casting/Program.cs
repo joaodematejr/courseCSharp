0af51b76dd0acfa6a0499a04f258cb5c1a9b312e
João Dematé Jr <joaodematejr@MacBook-Pro-de-Joao.local>
2020-12-09 21:41:29 -0300

    Casting

---
+++ casting/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4byte
+            float x = 4.5f;
+            //8byte
+            double y = x;
+            Console.WriteLine(y);
+
+            double a;
+            float b;
+
+            a = 5.1;
+            b = (float)a;
+
+            Console.whiteLine(b);
+
+            double c;
+            int d;
+
+            c = 5.1;
+            d = (int)c;
+
+            Console.whiteLine(d);
+
+            int e = 5;
+            int f = 2;
+
+            double resultado = (double)a / b;
+
+            Console.whiteLine(resultado);
+        }
+    }
+}
