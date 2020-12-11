d65ccf3bd6aed0238fdfa1b1544fd14e7c3aa18e
joaodematejr <joaodematejr@gmail.com>
2020-12-11 11:56:18 -0300

    escopo

---
+++ escopo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Escopo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(System.Console.WriteLine());
+            double desconto = 0.0;
+
+            if (preso > 100.0)
+            {
+                desconto = preco * 0.1;
+            }
+            
+            System.Console.WriteLine(desconto);
+        }
+    }
+}
