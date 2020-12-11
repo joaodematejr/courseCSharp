1556ebd5b4f4fac5d8e1d572d48df318cf6e6f2c
joaodematejr <joaodematejr@gmail.com>
2020-12-10 21:11:51 -0300

    Entrada de dados

---
+++ entradaDado/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace EntradaDado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+            string[] vet = Console.ReadLine().Split(" ");
+            string p1 = vet[0];
+            string p2 = vet[1];
+            string p3 = vet[2];
+
+
+        }
+    }
+}
