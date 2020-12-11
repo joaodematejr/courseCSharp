d70b6952706d70cdb22f8b99b3e3e4b577bb670f
joaodematejr <joaodematejr@gmail.com>
2020-12-10 21:40:33 -0300

    Entrada de dados 2

---
+++ entradaDadosInt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.WriteLine());
+            double n2 = double.Parse(Console.WriteLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2);
+
+            string[] vet = Console.ReadLine().Split(" ");
+            string nome = vet[0];
+            char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            double altura = double.Parse(vet[3], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
