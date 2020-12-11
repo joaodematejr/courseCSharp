ad625827e2cfb01619177463ea46007568cb72b8
joaodematejr <joaodematejr@gmail.com>
2020-12-10 22:55:03 -0300

    Operadores logicos

---
+++ operadoresLogicos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool c1 = 2 > 3 && 4 != 5; // TRUE
+            System.Console.WriteLine(c1);
+
+            bool c2 = !(2 > 3) && 4 != 5; //TRUE
+
+            System.Console.WriteLine(c2);
+
+            System.Console.WriteLine("_________");
+
+            bool c3 = 10 < 5; //FALSE
+            bool c4 = c1 || c2 && c3;  //TRUE
+
+        }
+    }
+}
