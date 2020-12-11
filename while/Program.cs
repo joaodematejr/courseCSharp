ffecfc44f11d14a7c23a5b4fc7e1337d671a8af2
joaodematejr <joaodematejr@gmail.com>
2020-12-11 16:35:17 -0300

    While

---
+++ while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            while (true)
+            {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture)); 
+                Console.Write("Digite outro número: ");
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);  
+            }
+
+            Console.Write("Número negativo");
+        }
+    }
+}
