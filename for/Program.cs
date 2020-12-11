86f1b6d00d64d61cfed21ada1126073af2c11a31
joaodematejr <joaodematejr@gmail.com>
2020-12-11 17:33:31 -0300

    for

---
+++ for/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos números inteiros você vai digitar ?");
+            int n = int.Parse(Console.ReadLine());
+            int soma = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Valor #{0}: ", i);  
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+            Console.Write("Soma = " + soma);
+        }
+    }
+}
