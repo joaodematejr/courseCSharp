c77fc35acdba39807707c59f43004cef2f44526e
joaodematejr <joaodematejr@gmail.com>
2020-12-15 14:58:33 -0300

    OO com metodo

---
+++ comOOV2/Triangulo.cs
@@ -0,0 +1,16 @@
+using System;
+namespace projectClass
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
