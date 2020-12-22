09fc9ec6500256dc166da95fbce1811ff45dbce2
joaodematejr <joaodematejr@gmail.com>
2020-12-22 15:32:06 -0300

    Struct

---
+++ struct/Point.cs
@@ -0,0 +1,14 @@
+using System.Globalization;
+namespace Course
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return "(" + X + ", " + Y + ")";
+        }
+    }
+}
