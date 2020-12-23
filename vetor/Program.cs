bb8cf592a33aa2c1b1606614c8c8c5a9753f2dc2
joaodematejr <joaodematejr@gmail.com>
2020-12-22 23:51:28 -0300

    Vetor

---
+++ vetor/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double[] vect = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+
+            double sum = 0.0; 
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+
+            double avg = sum / n;
+            Console.WriteLine("AVERAGE HEIGHT = " +avg.ToString("F2"), CultureInfo.InvariantCulture);
+        }
+    }
+}
