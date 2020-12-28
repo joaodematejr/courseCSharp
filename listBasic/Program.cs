79306f34ee20ced1e3bbbe4832a8af25fb6963d8
João Dematé Jr <joaodematejr@gmail.com>
2020-12-28 13:30:02 -0300

    Lista Basica

---
+++ listBasic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+
+            List<string> list2 = new List<string> { "Maria", "Alex" };
+        }
+    }
+}
