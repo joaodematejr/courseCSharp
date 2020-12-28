b391d0c2aae3d326be18432fd85ed87cf0d954d6
João Dematé Jr <joaodematejr@gmail.com>
2020-12-28 12:28:35 -0300

    Foreach

---
+++ foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vect = new string[] { "João", "Bob", "Patric", "Ana" };
+
+            foreach (var item in vect)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
