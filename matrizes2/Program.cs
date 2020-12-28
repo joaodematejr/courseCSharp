00ef1fbe1b8ce0bbba6c40b2819f6b26de37a134
João Dematé Jr <joaodematejr@gmail.com>
2020-12-28 20:58:27 -0300

    Matrizes 2

---
+++ matrizes2/Program.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+            Console.WriteLine("Main Diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.Write("Negativos numbers: " + count);
+        }
+    }
+}
