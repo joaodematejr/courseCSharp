9c33a732f5c5bb4af3a725957469ab335fdba4aa
João Dematé Jr <joaodematejr@gmail.com>
2020-12-29 16:36:06 -0300

    TimeSpan

---
+++ timeSpan/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t0 = new TimeSpan(0, 1, 30);
+
+            Console.WriteLine(t0);
+            Console.WriteLine(t0.Ticks);
+
+            TimeSpan t1 = new TimeSpan();
+            TimeSpan t2 = new TimeSpan(90000000L);
+            TimeSpan t3 = new TimeSpan(2, 11, 21);
+            TimeSpan t4 = new TimeSpan(1, 2, 11, 21);
+            TimeSpan t5 = new TimeSpan(1, 2, 11, 21, 321);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+
+            Console.WriteLine("---");
+
+            TimeSpan t6 = TimeSpan.FromDays(1.5);
+            TimeSpan t7 = TimeSpan.FromHours(1.5);
+            TimeSpan t8 = TimeSpan.FromMinutes(1.5);
+            TimeSpan t9 = TimeSpan.FromSeconds(1.5);
+            TimeSpan t10 = TimeSpan.FromMilliseconds(1.5);
+            TimeSpan t11 = TimeSpan.FromTicks(900000000L);
+
+            Console.WriteLine(t6);
+            Console.WriteLine(t7);
+            Console.WriteLine(t8);
+            Console.WriteLine(t9);
+            Console.WriteLine(t10);
+            Console.WriteLine(t11);
+        }
+    }
+}
