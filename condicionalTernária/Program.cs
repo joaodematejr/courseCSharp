8e63852bc3808f363160e655ec357b090557af84
João Dematé Jr <joaodematejr@gmail.com>
2020-12-29 14:25:03 -0300

    Condicional Ternária

---
+++ condicionalTernária/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            Console.WriteLine(desconto);
+        }
+    }
+}
