d31a098ca01f1c02747e3728931d3c3db71c66e9
João Dematé Jr <joaodematejr@gmail.com>
2020-12-29 14:08:11 -0300

    Var

---
+++ var/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "João";
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
