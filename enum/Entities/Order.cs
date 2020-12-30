f080b5a8ef364c3f6a2f8240f97018221a26e04c
João Dematé Jr <joaodematejr@gmail.com>
2020-12-30 14:07:21 -0300

    Enum

---
+++ enum/Entities/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using curso.Entities.Enums;
+
+namespace curso.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ", "
+                + Status;
+        }
+    }
+}
---
+++ enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using curso.Entities;
+using curso.Entities.Enums;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            Console.WriteLine("txt => " + txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine("os => " + os);
+        }
+    }
+}
