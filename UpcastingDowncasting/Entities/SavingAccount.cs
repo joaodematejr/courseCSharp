fe2547446bee30de53c7942a6cebc750da2b1457
João Dematé Jr <joaodematejr@gmail.com>
2020-12-31 14:17:05 -0300

    Upcasting e downcasting

---
+++ UpcastingDowncasting/Entities/SavingAccount.cs
@@ -0,0 +1,26 @@
+﻿namespace curso.Entities
+{
+    class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccount()
+        {
+        }
+
+        public SavingAccount(double interestRate)
+        {
+            InterestRate = interestRate;
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
---
+++ UpcastingDowncasting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using System.Globalization;
+using curso.Entities;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account acc = new Account(1001, "Alex", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //UPCASTING
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingAccount(1004, "Alex", 0.0, 0.01);
+
+            //DOWNCASTING
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+
+            //BusinessAccount acc5 = (BusinessAccount)acc3;
+
+            if (acc3 is BusinessAccount)
+            {
+                //BusinessAccount acc5 = (BusinessAccount)acc3;
+                BusinessAccount acc5 = acc3 as BusinessAccount;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan");
+            }
+
+            if (acc3 is SavingAccount)
+            {
+                SavingAccount acc5 = (SavingAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update");
+            }
+        }
+    }
+}
