5385c76673e2c8904ed20c45403519a1c004c45e
João Dematé Jr <joaodematejr@gmail.com>
2020-12-31 11:56:12 -0300

    Herança

---
+++ herança/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using System.Globalization;
+using curso.Entities;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+            //account.Balance = 200.0;
+        }
+    }
+}
