9e555426c832bdc082705d188af45862105fda37
João Dematé Jr <joaodematejr@gmail.com>
2021-01-03 00:33:09 -0300

    Sobreposição, Override, Virtual, Base

---
+++ override/Entities/SavingAccount.cs
@@ -0,0 +1,32 @@
+﻿namespace curso.Entities
+{
+    class SavingAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingAccount()
+        {
+        }
+
+        public SavingAccount(double interestRate)
+        {
+            InterestRate = interestRate;
+        }
+
+        public SavingAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
