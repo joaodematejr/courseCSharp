a68a59899121e13160c0bf307c8e16f5be2aa686
João Dematé Jr <joaodematejr@gmail.com>
2021-01-03 17:53:31 -0300

    Classe Abastratas

---
+++ classAbstratas/Entities/Account.cs
@@ -0,0 +1,31 @@
+﻿namespace curso.Entities
+{
+    abstract class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
---
+++ classAbstratas/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using curso.Entities;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> list = new List<Account>();
+            list.Add(new SavingAccount(1001, "Alex", 500.0, 0.01));
+            list.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            list.Add(new SavingAccount(1003, "Bob", 500.0, 0.01));
+            list.Add(new BusinessAccount(1002, "Anna", 500.0, 500.0));
+
+            double sum = 0.0;
+            foreach (Account acc in list)
+            {
+                sum += acc.Balance;
+            }
+
+            Console.WriteLine("Total balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach (Account acc in list)
+            {
+                acc.Withdraw(10.0);
+            }
+
+            foreach (Account acc in list)
+            {
+                Console.WriteLine("Updated balance for account "
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
