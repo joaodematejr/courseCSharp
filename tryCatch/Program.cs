c57688fbbfdb09be779ecb1be4c0027b0b850c69
João Dematé Jr <joaodematejr@gmail.com>
2021-01-04 13:58:58 -0300

    try catch

---
+++ tryCatch/Program.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not allowed");
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error ! " + e.Message);
+            }
+        }
+    }
+}
