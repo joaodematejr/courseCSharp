085e373429bec23fb271a61633d26632251f6f86
João Dematé Jr <joaodematejr@gmail.com>
2021-01-04 21:46:27 -0300

    FileStream e StreamReader

---
+++ fileStreamStreamReader/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"/Users/joaodematejr/Documents/github/courseCSharp/temp/file1.txt";
+
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("An error accurred");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+
+        }
+    }
+}
