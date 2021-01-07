e4ade908e6b01f3b322a1a005df1f70fc845caa5
João Dematé Jr <joaodematejr@gmail.com>
2021-01-06 21:21:35 -0300

    Aula de Interfaces solução 01 sem ultilizar interface

---
+++ exercicio04/Services/BrazilTaxService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Curso.Services
+{
+    class BrazilTaxService
+    {
+        public BrazilTaxService()
+        {
+        }
+
+        public double Tax(double amount)
+        {
+            if (amount <= 100)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    }
+}
---
+++ exercicio04/Services/RentalService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Curso.Entities;
+
+namespace Curso.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; set; }
+
+        private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
+        public RentalService(double hour, double day)
+        {
+            PricePerHour = hour;
+            PricePerDay = day;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+
+            if (duration.TotalHours <= 12.0)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
