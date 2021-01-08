f5e3f5573304531fd95c40bcc56274299feea99b
João Dematé Jr <joaodematejr@gmail.com>
2021-01-08 13:23:24 -0300

    Extension methods 2

---
+++ extensionMethods2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            string s1 = "Good morning dear students !";
+
+            Console.WriteLine(s1.Cut(5));
+        }
+    }
+}
