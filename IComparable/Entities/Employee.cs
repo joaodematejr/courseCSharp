611397deaeb3439a1fb3a03bcc61bddcb4340d45
João Dematé Jr <joaodematejr@gmail.com>
2021-01-08 11:26:44 -0300

    202. Interface IComparable

---
+++ IComparable/Entities/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso4.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(",");
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
