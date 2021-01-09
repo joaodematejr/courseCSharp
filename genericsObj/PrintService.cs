1f8c914cb2331f7f6b2d8e96b849a27cd9f2449e
João Dematé Jr <joaodematejr@gmail.com>
2021-01-08 22:54:30 -0300

    Generics obj

---
+++ genericsObj/PrintService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Curso
+{
+    class PrintService
+    {
+        private object[] _values = new object[10];
+        private int _count = 0;
+
+        public void AddValue(object value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public object First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("PrintService is empty");
+            }
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+                Console.WriteLine("]");
+            }
+        }
+
+
+    }
+}
---
+++ genericsObj/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+
+            Console.Write("How many values ? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+
+            int a = (int)printService.First();
+            int b = a + 2;
+            Console.WriteLine(b);
+
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+}
