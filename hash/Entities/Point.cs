aee8da669eee852f512a1595cbe031035ce76cfc
João Dematé Jr <joaodematejr@gmail.com>
2021-01-10 15:58:37 -0300

    Implementação comparaões entre hash

---
+++ hash/Entities/Point.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Curso.Entities
+{
+    struct Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int x, int y) : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
---
+++ hash/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Curso.Entities;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<Product> a = new HashSet<Product>();
+            a.Add(new Product("TV", 900.0));
+            a.Add(new Product("Notebook", 1900.0));
+
+            HashSet<Point> b = new HashSet<Point>();
+
+            b.Add(new Point(3, 4));
+            b.Add(new Point(5, 10));
+
+            Product prod = new Product("Notebook", 1900.0);
+            Console.WriteLine(a.Contains(prod));
+
+            Point p = new Point(5, 10);
+            Console.WriteLine(b.Contains(p));
+
+        }
+    }
+}
