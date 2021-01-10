63d5e24f73a0c2cfead5f1bbe7e238cd7773c0ba
João Dematé Jr <joaodematejr@gmail.com>
2021-01-10 14:16:29 -0300

    HashSet

---
+++ hashSet/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Table");
+
+            Console.WriteLine(set.Contains("Notebook"));
+
+            foreach (string p in set)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+    }
+}
