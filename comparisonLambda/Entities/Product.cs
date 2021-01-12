633a697ffe6f86d03cfd8890085d607352d50634
João Dematé Jr <joaodematejr@gmail.com>
2021-01-12 14:34:57 -0300

    219. Uma experiência com Comparison

---
+++ comparisonLambda/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ comparisonLambda/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Curso.Entities;
+using System.Collections.Generic;
+
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.0));
+            list.Add(new Product("Notebook", 900.0));
+            list.Add(new Product("Tablet", 900.0));
+
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+    }
+}
