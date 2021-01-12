7d9fa1cead27dcf7ac29f58e84f106e38fe01d06
João Dematé Jr <joaodematejr@gmail.com>
2021-01-12 00:15:06 -0300

    Finalização conjunto

---
+++ exercicioConjuntos/Entities/LogRecord.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Curso.Entities
+{
+    public class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ exercicioConjuntos/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+using Curso.Entities;
+
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime instante = DateTime.Parse(line[1]);
+                        set.Add(new LogRecord { Username = name, Instant = instante });
+    
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
