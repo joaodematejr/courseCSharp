b466454e7abd5cdfb36e3d7edc980735a1e7e84b
João Dematé Jr <joaodematejr@gmail.com>
2021-01-12 22:32:30 -0300

    223. Predicate (exemplo com RemoveAll)

---
+++ predicateRemove/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(ProductTest);
+            //or
+            //list.RemoveAll(p => p.Price >= 100.0);
+
+            foreach (Product p in list)
+            {
+                System.Console.WriteLine(p);
+            }
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.00;
+        }
+    }
+}
