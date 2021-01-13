3cf47ab8b1020603d443a0d2ac44e08c25367cf1
João Dematé Jr <joaodematejr@gmail.com>
2021-01-12 21:47:31 -0300

    222. Multicast delegates

---
+++ multicastDelegates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Curso.Services;
+
+
+namespace Curso
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+        }
+    }
+}
