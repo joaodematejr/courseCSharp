3c1b3cad767fd564ebfd93a89c5355139c4b7b65
João Dematé Jr <joaodematejr@gmail.com>
2021-01-12 21:26:34 -0300

    221. Introdução a delegates

---
+++ delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Curso.Services;
+
+
+namespace Curso
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op.Invoke(a, b);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ delegates/Services/CalculationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Curso.Services
+{
+    class CalculationService
+    {
+        public static double Max(double x, double y)
+        {
+            return (x > y) ? x : y;
+        }
+
+        public static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Square(double x)
+        {
+            return x * x;
+        }
+    }
+}
