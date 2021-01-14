ce61eba2513d38872bc5b3ac4cf204a3f2f6a1ea
João Dematé Jr <joaodematejr@gmail.com>
2021-01-13 23:37:31 -0300

    225. Func (exemplo com Select)

---
+++ func/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD Case", 80.90));
+
+            //List<string> result = list.Select(NameUpper).ToList();
+
+            //Func<Product, string> func = NameUpper;
+
+            //Func<Product, string> func = p => p.Name.ToUpper();
+
+            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+       // static string NameUpper(Product p)
+       // {
+       //     return p.Name.ToUpper();
+       // }
+    }
+}
