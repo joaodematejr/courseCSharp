a41a1cc2fb07733a38e3da5b04cbaa03c48a2735
João Dematé Jr <joaodematejr@gmail.com>
2021-01-15 23:23:49 -0300

    Linq 3

---
+++ linq3/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Curso.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Category Category { get; set; }
+
+        public Product()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Name
+                + ", "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Category.Name
+                + ", "
+                + Category.Tier;
+        }
+    }
+}
