ab3b2710496ab4ca300584b4c46b27085206ba14
João Dematé Jr <joaodematejr@gmail.com>
2021-01-17 16:13:16 -0300

    Criação classe posição

---
+++ xadrez/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+
+namespace Curso
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+        }
+    }
+}
---
+++ xadrez/tabuleiro/Posicao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    public class Posicao
+    {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao()
+        {
+        }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return linha + ", " + coluna;
+        }
+    }
+}
