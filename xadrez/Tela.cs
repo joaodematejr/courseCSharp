b16650647e3662562b8366197cc6be655df0e493
João Dematé Jr <joaodematejr@gmail.com>
2021-01-18 21:37:47 -0300

    Melhorando a implementação do metodo imprimir Tabuleiro

--- xadrez/Tela.cs
+++ xadrez/Tela.cs
@@ -9,6 +9,7 @@ namespace Curso
         {
             for (int i = 0; i < tab.linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
@@ -17,11 +18,29 @@ namespace Curso
                     }
                     else
                     {
-                        Console.Write(" " + tab.peca(i, j) + " ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
+                        //Console.Write(" " + tab.peca(i, j) + " ");
                     }
 
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine("   A  B  C  D  E  F  G  H ");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(" " + peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(" " + peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
