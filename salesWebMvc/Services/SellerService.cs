07118d1e2d475f371a3bcf683425e5591d0b50c6
João Dematé Jr <joaodematejr@gmail.com>
2021-01-30 23:56:45 -0300

    Seller Service and basic Find All

--- salesWebMvc/Services/SellerService.cs
+++ salesWebMvc/Services/SellerService.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SalesWebMvc.Models;
+
 namespace SalesWebMvc.Services
 {
     public class SellerService
     {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
         public SellerService()
         {
         }
