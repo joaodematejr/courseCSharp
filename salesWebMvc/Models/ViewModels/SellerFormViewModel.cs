fa6d5ad7f122972bcd1091ecb20086c84db7f18f
João Dematé Jr <joaodematejr@gmail.com>
2021-02-03 23:23:16 -0300

    SellerFormViewModel and Department select component

---
+++ salesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public SellerFormViewModel()
+        {
+        }
+    }
+}
---
+++ salesWebMvc/Services/DepartmentService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SalesWebMvc.Services
+{
+    public class DepartmentService
+    {
+        public DepartmentService()
+        {
+        }
+    }
+}
