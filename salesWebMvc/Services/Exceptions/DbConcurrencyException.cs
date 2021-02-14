7445dd1ed04ea4d855bcff9567afbcd1495613ff
João Dematé Jr <joaodematejr@gmail.com>
2021-02-14 17:54:50 -0300

    Update seller and custom service exception

---
+++ salesWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrencyException
+    {
+        public DbConcurrencyException()
+        {
+        }
+    }
+}
---
+++ salesWebMvc/Views/Sellers/Edit.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SalesWebMvc.Views.Sellers
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
